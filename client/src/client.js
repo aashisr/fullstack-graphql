cd2d6f4b8c0998be341e7eefb0620ff5f45440fb
Aashis Rimal <aashis_rimal@yahoo.com>
2019-12-27 23:06:43 +0200

    Added a graphql client and integrated it to react

--- client/src/client.js
+++ client/src/client.js
@@ -6,3 +6,16 @@ import gql from 'graphql-tag'
 /**
  * Create a new apollo client and export as default
  */
+
+const link = new HttpLink({ uri: 'http://localhost:4000/' });
+
+// Cache 
+const cache = new InMemoryCache();
+
+// Initialize a Client
+const client = new ApolloClient({
+	link,
+	cache
+});
+
+export default client;
--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { ApolloProvider } from '@apollo/react-hooks'
 import App from './components/App'
+import client from './client';
 import './index.css'
 
 const Root = () => (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+    <BrowserRouter>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </BrowserRouter>
+);
 
 ReactDOM.render(<Root />, document.getElementById('app'))
 
